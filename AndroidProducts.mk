45f3bb7556a6eee7c7594cf4bdaf9369b8649f92
ItoRenz <itorenz@nusantararom.org>
2022-08-07 12:42:14 +0000

    rova: Syberia bringup
    Signed-off-by: ItoRenz <itorenz@nusantararom.org>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,7 @@
-#
-# Copyright (C) 2021 The LineageOS Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/nad_rova.mk
+    $(LOCAL_DIR)/syberia_rova.mk
 
 COMMON_LUNCH_CHOICES := \
-    nad_rova-user \
-    nad_rova-userdebug \
-    nad_rova-eng
+    syberia_rova-user \
+    syberia_rova-userdebug \
+    syberia_rova-eng
