ca631804f29857b5bbd54a7ceae08f6b975e44d7
Maxx1221 <somanathcharan9@gmail.com>
2023-04-23 05:46:02 +0000

    Revert "Merge branch 'a13-cherish' into a13"
    This reverts commit 5dd822ea66a3143f168c15448523034eedf6ccde, reversing
    changes made to ad4a58277735e784e89c0f5e4dd0e46ac49371a4.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/cherish_rova.mk
+    $(LOCAL_DIR)/aosp_rova.mk
 
 COMMON_LUNCH_CHOICES := \
-    cherish_rova-user \
-    cherish_rova-userdebug \
-    cherish_rova-eng
+    aosp_rova-user \
+    aosp_rova-userdebug \
+    aosp_rova-eng
--- aosp_rova.mk
+++ aosp_rova.mk
@@ -13,19 +13,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk
 TARGET_BOOT_ANIMATION_RES := 720
 USE_PIXEL_CHARGING := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
-$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
-
-# Inherit some props from Cherish
-CHERISH_VANILLA := true
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.cherish.maintainer= @maxx459
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from rova device
 $(call inherit-product, device/xiaomi/rova/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := rova
-PRODUCT_NAME := cherish_rova
+PRODUCT_NAME := aosp_rova
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 4A / 5A
